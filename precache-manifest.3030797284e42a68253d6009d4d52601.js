764ce3dea8c6b3a971e5e1c3d63124bc36747775
Mudassir Sayed <sayedmudassir37@gmail.com>
2020-02-17 22:13:23 +0530

    Updates

---
+++ precache-manifest.3030797284e42a68253d6009d4d52601.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ca361288e71679a238fd103190a0dfb",
+    "url": "/nkproject/index.html"
+  },
+  {
+    "revision": "75c8dc0f3362fd2858f0",
+    "url": "/nkproject/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "f84ee78ee3fcc3fcbc83",
+    "url": "/nkproject/static/css/main.62c51538.chunk.css"
+  },
+  {
+    "revision": "75c8dc0f3362fd2858f0",
+    "url": "/nkproject/static/js/2.a7cc56e9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/nkproject/static/js/2.a7cc56e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f84ee78ee3fcc3fcbc83",
+    "url": "/nkproject/static/js/main.bb4a0830.chunk.js"
+  },
+  {
+    "revision": "cb05e7c4c8a57da5404b",
+    "url": "/nkproject/static/js/runtime-main.1f01f6df.js"
+  }
+]);
